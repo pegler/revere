88c97b04f9edc2d7839b308967510c18d38ecb7d
Arik Fraimovich <arik@arikfr.com>
2014-01-30 19:08:27 +0200

    Use wtf-alchemy, as model_form wasn't compatible with latest sqlalchemy

--- revere/forms.py
+++ revere/forms.py
@@ -1,40 +1,45 @@
 from flask.ext.wtf import Form
 from wtforms import validators
-from wtforms.ext.sqlalchemy.orm import model_form
+from wtforms_alchemy import model_form_factory
 from revere.db import Alert, Monitor
 
-AlertForm = model_form(Alert,
-                       base_class=Form,
-                       exclude=['id', 'key'])
 
-MonitorForm = model_form(Monitor, base_class=Form,
-                        exclude=['id', 'state', 'logs'],
-                        field_args={
-                            'schedule_year': {
-                                'validators': [validators.Length(min=4, max=50)]
-                            },
-                            'schedule_year': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_month': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_day': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_week': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_day_of_week': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_second': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_minute': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                            'schedule_second': {
-                                'validators': [validators.Length(min=1, max=10)]
-                            },
-                        })
+ModelForm = model_form_factory(Form)
+
+
+class AlertForm(ModelForm):
+    class Meta:
+        model = Alert
+        exclude = ['key']
+
+
+class MonitorForm(ModelForm):
+    class Meta:
+        model = Monitor
+        exclude = ['state']
+        field_args = {
+            'schedule_year': {
+                'validators': [validators.Length(min=1, max=50)]
+            },
+            'schedule_month': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+            'schedule_day': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+            'schedule_week': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+            'schedule_day_of_week': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+            'schedule_second': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+            'schedule_minute': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+            'schedule_second': {
+                'validators': [validators.Length(min=1, max=10)]
+            },
+        }
